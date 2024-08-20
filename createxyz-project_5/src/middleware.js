54d510d36a1543c9bda326ccf4043b14247b7ca2
go2020h <go2020.hidane.inc@gmail.com>
2024-08-20 15:08:19 +0900

    test
    test1

---
+++ createxyz-project_5/src/middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: "/integrations/:path*",
+};
+
+export function middleware(request) {
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-createxyz-project-id", "68561965-74c5-4f2c-9955-2ebdbd2d6d4e");
+
+  request.nextUrl.href = `https://www.create.xyz/${request.nextUrl.pathname}`;
+
+  return NextResponse.rewrite(request.nextUrl, {
+    request: {
+      headers: requestHeaders,
+    },
+  });
+}
